4aecd65239eb3e3d8b6152d93c834537aa5a5fad
mhollstein <56024424+mhollstein@users.noreply.github.com>
2022-02-04 08:48:08 +0100

    Create do_smartmon.sh

---
+++ do_smartmon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run smartmon.sh and create a temporäry *.prom file
+# mv  temoräry *.prom file to destination for textfile_collector
+# reason: textfile_collector should be able to read the *.prom file at any time
+
+# create metrics                   in this file                                   move this file to destination to be imported by textfile_collector, node_exporter, Prometheus
+/opt/smart_exporter/smartmon.sh > /opt/smart_exporter/smart_metrics.prom 2>&1  && sudo mv -f /opt/smart_exporter/smart_metrics.prom /service/node_exporter/textfile_collector/smart_metrics.prom 2>&1
